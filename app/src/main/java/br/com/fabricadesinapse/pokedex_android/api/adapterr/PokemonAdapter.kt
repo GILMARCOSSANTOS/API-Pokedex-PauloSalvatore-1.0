1b5f0948355b729a9ca5136c06a6797e28032f5a
Gilmarcos Santos <GILMARCOSXSANTOS@GMAIL.COM>
2022-12-01 00:12:54 -0300

    Salvar

--- app/src/main/java/br/com/fabricadesinapse/pokedex_android/api/adapterr/PokemonAdapter.kt
+++ app/src/main/java/br/com/fabricadesinapse/pokedex_android/api/adapterr/PokemonAdapter.kt
@@ -14,20 +14,6 @@ class PokemonAdapter(
     private val items: List<Pokemon?>
 ) : RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.pokemon_item, parent, false)
-
-        return ViewHolder(view)
-    }
-
-    override fun getItemCount() = items.size
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = items[position]
-
-        holder.bindView(item)
-    }
-
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bindView(item: Pokemon?) = with(itemView) {
             val ivPokemon = findViewById<ImageView>(R.id.ivPokemon)
@@ -52,4 +38,20 @@ class PokemonAdapter(
             }
         }
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.pokemon_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+
+        holder.bindView(item)
+    }
+
+
 }
